809d96676cef8bee139d24a5a4990b67b8663d57
Christian Larrea <crlarrea@protonmail.com>
2024-06-17 10:54:50 +0200

    add style to active menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/images/logo.webp";
 export const Nav = () => {
   const nav = ["tours", "merch", "music", "about", "gallery"];
+  const [selection, setSelection] = useState([]);
   return (
     <nav>
       <img src={logo} alt="" />
       <ul>
         {nav.map((el) => {
           return (
-            <li key={el}>
-              <NavLink to={el}>{el}</NavLink>
+            <li
+              key={el}
+              onClick={() => {
+                setSelection(el);
+              }}
+            >
+              <NavLink
+                to={el}
+                className={el === selection ? "active" : ""}
+              >
+                {el}
+              </NavLink>
             </li>
           );
         })}
