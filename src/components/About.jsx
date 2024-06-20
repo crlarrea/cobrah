8ee6bd2ab5bd243ca6a1b695b8c4d11342eed298
Christian Larrea <crlarrea@protonmail.com>
2024-06-20 07:50:44 +0200

    update header about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <section className="about">
       <article>
-        <h2>COBRAH</h2>
+        <h2>Meet COBRAH</h2>
         <p>
           Meet COBRAH, the electrifying Swedish artist redefining the music
           scene with her genre-blending, unapologetically bold sound! Bursting
